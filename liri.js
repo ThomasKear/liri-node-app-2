22bd83c49a66983e8e0ae5148261945bd17c0fb4
Thomas Kearney <kearnage1975@gmail.com>
2017-04-27 16:12:58 -0400

    final predouction before turn in

--- liri.js
+++ liri.js
@@ -1,25 +1,39 @@
 // I built different console.log catergories to focus the information being relayed
-var spotDebug = true;
-var twittDebug = false;
-var movieDebug = false;
-var randomDebug = true;
+var spotDebug = 1;
+var twittDebug = 1;
+var movieDebug = 1;
+var randomDebug = 1;
+var generalDebug = 1;
+var answersDebug = 0;
+
 // vars for twitter API
 var Twitter = require('twitter');
 var twittKeys = require("./keys.js");
 var client = new Twitter(twittKeys.twitterKeys);
 var params = { q: 'kearnage1975', count: 20 };
+
 // var for spotify API
 var spotify = require('spotify');
+
 // var for function request for the OMDB API
 var request = require('request');
-// vars for reading users commands
+
+// vars for reading users command
 var liriCommand = process.argv[2];
+
+// var for reading the user's entire command line for later use
 var nodeArg = process.argv;
 
+// this var is to log the user's commands in log.txt
+var logUserEntry = "";
+
+// this var is required to read/write from other files
 var fs = require("fs");
 
+// This is the title 
 var searchTitle = "";
-// loppoing through to ensure we capture the entire entry field
+
+// looping through to ensure we capture the users title search even if it is a multi-word search
 for (var i = 3; i < nodeArg.length; i++) {
     if (i > 3 && i < nodeArg.length) {
         searchTitle = searchTitle + "+" + nodeArg[i];
@@ -28,12 +42,23 @@ for (var i = 3; i < nodeArg.length; i++) {
     }
 }
 
-fs.appendFile('log.txt', nodeArg, function(err) {
-	if (err) {
-		console.log(err)
-	}
-});
+// looping through to capture the user's commands to log 
+for (var j = 2; j < nodeArg.length; j++) {
+    if (j > 2 && j < nodeArg.length) {
+        logUserEntry = logUserEntry + "+" + nodeArg[i];
+    } else {
+        logUserEntry += nodeArg[j];
+    }
+}
 
+// this is to read the user's commands and log them to the log.txt page
+fs.appendFile('log.txt', logUserEntry, function(err) {
+    if (err) {
+        generalPrint(err)
+    } else {
+        generalPrint(logUserEntry)
+    }
+});
 
 // this dicates what function goes to what command the user entered
 switch (liriCommand) {
@@ -54,7 +79,7 @@ switch (liriCommand) {
         break;
 }
 
-// This is the my-tweets responce function
+// This is the my-tweets response function
 function myTweets() {
     client.get('search/tweets', params, gotData);
 
@@ -64,15 +89,16 @@ function myTweets() {
             if (error) {
                 twittPrint(error);
             } else {
-                console.log(tweets[i].created_at);
-                console.log(tweets[i].text);
+                answersPrint(tweets[i].created_at);
+                answersPrint(tweets[i].text);
             }
         }
     }
 }
 
-
+// This is the spotify-this--song response function
 function myPlayList() {
+    // if the user chooses nothing, then Ace of Base is chosen
     if (!searchTitle) {
         searchTitle = 'The Sign by Ace of Base';
     }
@@ -87,18 +113,19 @@ function myPlayList() {
         var albumTrack = data.tracks.items;
         // spotPrint('albumTrack: ' + albumTrack)
         for (var i = 0; i < albumTrack.length; i++) {
-            console.log("Artist: " + albumTrack[i].artists[0].name);
-            console.log("Album Title: " + albumTrack[i].album.name);
-            console.log("Spotify Link: " + albumTrack[i].preview_url);
-            console.log("Track Title: " + albumTrack[i].name);
+            answersPrint("Artist: " + albumTrack[i].artists[0].name);
+            answersPrint("Album Title: " + albumTrack[i].album.name);
+            answersPrint("Spotify Link: " + albumTrack[i].preview_url);
+            answersPrint("Track Title: " + albumTrack[i].name);
 
         }
     });
 }
 
-
+// this is the movie-this response function
 function myMovie() {
 
+    // if a movie title isn't called upon, "Mr. Nobody" is called upon
     if (!searchTitle) {
         searchTitle = 'Mr. Nobody';
     }
@@ -115,20 +142,22 @@ function myMovie() {
         if (!error && response.statusCode === 200) {
 
             // Parse the body of the site and recover just the imdbRating
-            console.log("Movie Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Country Movie Was Produced: " + JSON.parse(body).Country);
-            console.log("Movie Language: " + JSON.parse(body).Language);
-            console.log("Movie Plot: " + JSON.parse(body).Plot);
-            console.log("Movie Actor: " + JSON.parse(body).Actors);
-            console.log("Rotten Tomatoes URL: " + rotTomaUrl;
+            answersPrint("Movie Title: " + JSON.parse(body).Title);
+            answersPrint("Release Year: " + JSON.parse(body).Year);
+            answersPrint("IMDB Rating: " + JSON.parse(body).imdbRating);
+            answersPrint("Country Movie Was Produced: " + JSON.parse(body).Country);
+            answersPrint("Movie Language: " + JSON.parse(body).Language);
+            answersPrint("Movie Plot: " + JSON.parse(body).Plot);
+            answersPrint("Movie Actor: " + JSON.parse(body).Actors);
+            answersPrint("Rotten Tomatoes URL: " + rotTomaUrl);
         }
 
     });
 }
 
+// this this is the do-what-it-says response function
 function randomPick() {
+
     fs.readFile("random.txt", "utf8", function(error, data) {
         randomPrint(error);
         // We will then print the contents of data
@@ -137,52 +166,64 @@ function randomPick() {
         var dataArr = data.split(",");
         // We will then re-display the content as an array for later use.
         randomPrint(dataArr[1]);
+
         var randomTitle = dataArr[1];
         randomPrint(randomTitle);
         spotify.search({ type: 'track', query: randomTitle }, function(err, data) {
-        if (err) {
-            spotPrint('Error occurred: ' + err);
-            return;
-        }
-        // spotPrint(data);
-        // Handle Data
-        var albumTrack = data.tracks.items;
-        // spotPrint('albumTrack: ' + albumTrack)
-        for (i = 0; i < albumTrack.length; i++) {
-            console.log("Artist: " + albumTrack[i].artists[0].name);
-            console.log("Album Title: " + albumTrack[i].album.name);
-            console.log("Spotify Link: " + albumTrack[i].preview_url);
-            console.log("Track Title: " + albumTrack[i].name);
+            if (err) {
+                spotPrint('Error occurred: ' + err);
+                return;
+            }
+            // Handle Data
+            var albumTrack = data.tracks.items;
+            // spotPrint('albumTrack: ' + albumTrack)
+            for (i = 0; i < albumTrack.length; i++) {
+                answersPrint("Artist: " + albumTrack[i].artists[0].name);
+                answersPrint("Album Title: " + albumTrack[i].album.name);
+                answersPrint("Spotify Link: " + albumTrack[i].preview_url);
+                answersPrint("Track Title: " + albumTrack[i].name);
 
-        }
-    });
+            }
+        });
     })
 }
 
 
 // console.log("searchTitle: " +  searchTitle);
 
-// debugPrint only prints when var globaDebug * is set to true. Useful for printing backend logic information when debugging. @param {string} the message you wish to print
+// debugPrint only prints when var globaDebug * is set to 0. Useful for printing backend logic information when debugging. @param {string} the message you wish to print
 function twittPrint(msg) {
-    if (twittDebug == true) {
+    if (!twittDebug) {
         console.log(msg)
     }
 }
 
 function spotPrint(msg) {
-    if (spotDebug == true) {
+    if (!spotDebug) {
         console.log(msg)
     }
 }
 
 function moviePrint(msg) {
-    if (movieDebug == true) {
+    if (!movieDebug) {
         console.log(msg)
     }
 }
 
 function randomPrint(msg) {
-    if (randomDebug == true) {
+    if (!randomDebug) {
+        console.log(msg)
+    }
+}
+
+function generalPrint(msg) {
+    if (!generalDebug) {
+        console.log(msg)
+    }
+}
+
+function answersPrint(msg) {
+    if (!answersDebug) {
         console.log(msg)
     }
 }
